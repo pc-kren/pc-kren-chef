65b2ecf16bf66c8cfa961763db219fc7f6b98680
pc-kren <ken.ren@personalcapital.com>
2012-09-24 23:37:08 +0000

    add package

--- cookbooks/kren/attributes/default.rb
+++ cookbooks/kren/attributes/default.rb
@@ -1,6 +1,6 @@
 case node['plateform']
   when 'centos','rtel'
-    default[`kren`]['apache_package_name'] = 'httpd'
+    default['kren']['apache_package_name'] = 'httpd'
   when 'ubuntu','debian'
     default['kren']['apache_package_name'] = 'apache2'
 end
