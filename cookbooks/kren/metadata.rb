07f99889d3ab2915a3ccb4facd4ca13dee6b1bd8
pc-kren <ken.ren@personalcapital.com>
2012-09-24 20:47:56 +0000

    commit

--- cookbooks/kren/metadata.rb
+++ cookbooks/kren/metadata.rb
@@ -1,6 +1,9 @@
 maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer_email "kren@personalcapital.com"
 license          "All rights reserved"
 description      "Installs/Configures kren"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+
+recipe "kren::helloworld", "Prints hello world"
